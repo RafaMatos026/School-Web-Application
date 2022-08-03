ae0beb5ffdc1e7a8ff86eaf7cd86049c1c4335a5
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-03 18:20:59 +0100

    Add files via upload

--- client/src/views/Admin/AcceptRegistrations.tsx
+++ client/src/views/Admin/AcceptRegistrations.tsx
@@ -1,14 +1,37 @@
-import { Grid, Toolbar, Typography } from "@mui/material";
-import Box from '@mui/material/Box';
+import { Button, Grid, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 export default function AcceptRegistration() {
 
     return (
         <div>
             <Toolbar />
-            <Box display='flex' justifyContent={'center'} sx={{ width: 1 }}>
-                Hello World
-            </Box>
+            <Grid container display='flex' justifyContent={'center'} alignItems='center'>
+                <Grid item boxShadow={3} sx={{width: 3/4}} display='flex' justifyContent={'space-evenly'} alignItems='center' marginTop={1} padding={2}>
+                    <Box>
+                        <Typography variant="button" fontSize={16} fontWeight='500'>Request 1</Typography>
+                    </Box>
+                    <Box display={'inline'}>
+                        <Typography variant="button" fontSize={16} fontWeight='500'>
+                            Professor: &nbsp;
+                        </Typography>
+                        <Typography variant="button" fontSize={14} fontWeight={400}>
+                            Ola
+                        </Typography>
+                    </Box>
+                    <Box display={'inline'}>
+                        <Typography variant="button" fontSize={16} fontWeight='500'>
+                            Status: &nbsp;
+                        </Typography>
+                        <Typography variant="button" fontSize={14} fontWeight={400}>
+                            Ola
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Button variant="contained">View Request</Button>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
     )
 }
