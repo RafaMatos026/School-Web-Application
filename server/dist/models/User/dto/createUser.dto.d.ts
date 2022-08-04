3848775ca77d1b09c3dc4979836a7c4bf158d0f9
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-04 12:08:45 +0100

    Delete server directory

--- server/dist/models/User/dto/createUser.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { Subject } from 'src/models/Subject/subject.schema';
-export declare class CreateUserDto {
-    FName: string;
-    LName: string;
-    Email: string;
-    Birthday: Date;
-    AccountType: Subject;
-    Password: string;
-}
--- server/src/models/Summary/dto/createSummary.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Class } from 'src/models/Aula/class.schema';
-
-export class CreateSummaryDto {
-  Date: Date;
-  Description: string;
-  classId: Class;
-}
