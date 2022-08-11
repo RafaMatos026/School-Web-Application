322e9a493308456339ea29440298528fe39bf5f0
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-11 08:52:41 +0100

    Add files via upload

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3001);
   console.log('Listening on port 3001');
 }
