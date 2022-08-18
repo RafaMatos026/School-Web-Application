b9179aee46bf0963555116a3a799dd6e78df442e
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-18 11:08:04 +0100

    Add files via upload

--- server/src/models/Aula/class.module.ts
+++ server/src/models/Aula/class.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../User/user.module';
 import { ClassController } from './class.controller';
 import { Class, ClassSchema } from './class.schema';
 import { ClassService } from './class.service';
@@ -7,6 +8,7 @@ import { ClassService } from './class.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Class.name, schema: ClassSchema }]),
+    UserModule,
   ],
   controllers: [ClassController],
   providers: [ClassService],
