989cad449cd33dc911022d3682d00b96ea4a6f5e
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-25 11:03:48 +0100

    adding env vars
    added env vars

--- server/dist/auth/strategies/jwt.strategy.d.ts
+++ server/dist/auth/strategies/jwt.strategy.d.ts
@@ -1,5 +1,5 @@
-import { Strategy } from 'passport-jwt';
-import { UserPayload } from '../models/UserPayload';
+import { Strategy } from "passport-jwt";
+import { UserPayload } from "../models/UserPayload";
 declare const JwtStrategy_base: new (...args: any[]) => Strategy;
 export declare class JwtStrategy extends JwtStrategy_base {
     constructor();
