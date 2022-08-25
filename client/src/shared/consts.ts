e033f9fde4218fb7e7b10a4c79d2103efdb6e24f
RafaMatos026 <83010438+RafaMatos026@users.noreply.github.com>
2022-08-25 11:25:56 +0100

    New url for the backend

--- client/src/shared/consts.ts
+++ client/src/shared/consts.ts
@@ -1,4 +1,7 @@
 export const admin = "62f38d82cafa8d86f57141c1";
 export const student = "62f38d97cafa8d86f57141c5";
 export const teacher = "62f38d8ccafa8d86f57141c3";
-export const BASE_URL = process.env.NODE_ENV ==="development" ? "http://localhost:3001" : "school-web-application-backend.vercel.app"
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "school-web-application-backend.vercel.app";
